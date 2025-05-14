c0e05037a596a7f4a2723942f848f0226e32e609
fyam <fung19971115@gmail.com>
2025-05-14 15:05:17 +0800

    Setup vuetify and icons

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,36 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
+// Vuetify
+import 'vuetify/styles'
+import {createVuetify} from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import * as labsComponents from 'vuetify/labs/components'
 
-createApp(App).mount('#app')
+// Icons
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import {aliases, md} from 'vuetify/iconsets/md'
+
+
+const vuetify = createVuetify(
+    {
+        components: {
+            ...components,
+            ...labsComponents,
+        },
+        directives: directives,
+        icons: {
+            defaultSet: 'md',
+            aliases,
+            sets: {
+                md,
+            },
+        },
+        theme: {
+            defaultTheme: 'dark',
+        },
+    }
+)
+
+
+createApp(App).use(vuetify).mount('#app')
