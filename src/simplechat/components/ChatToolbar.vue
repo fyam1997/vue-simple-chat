ea2e813050fb229241bcb1bf2e0c90f78a682d8d
fyam <fung19971115@gmail.com>
2025-05-15 13:17:57 +0800

    Add responsive behavior to ChatToolbar for better mobile support

--- src/simplechat/components/ChatToolbar.vue
+++ src/simplechat/components/ChatToolbar.vue
@@ -1,16 +1,20 @@
 <script setup lang="ts">
 
 import ChatConfigDialog, {ChatConfigModel} from "@/simplechat/components/ChatConfigDialog.vue";
+import {useWindowSize} from '@vueuse/core';
+import {computed} from "vue";
 
 defineProps<{
   apiConfig: ChatConfigModel,
   darkTheme: boolean,
-}>();
+}>()
 const emits = defineEmits<{
   openBugReport: [],
   downloadChats: [],
   toggleDownTheme: [],
 }>()
+const screenWidth = useWindowSize().width
+const largeScreen = computed(() => screenWidth.value > 425)
 
 </script>
 
@@ -19,7 +23,7 @@ const emits = defineEmits<{
     <template v-slot:title>
       <ChatConfigDialog :api-config="apiConfig"/>
     </template>
-    <template v-slot:append>
+    <template v-slot:append v-if="largeScreen">
       <v-btn
           :icon="darkTheme ? 'md:light_mode' : 'md:dark_mode'"
           variant="plain"
@@ -38,6 +42,38 @@ const emits = defineEmits<{
           @click="emits('downloadChats')"
           title="download chats"
       />
+    </template>
+    <template v-slot:append v-if="!largeScreen">
+      <v-menu>
+        <template v-slot:activator="{ props }">
+          <v-btn
+              icon="md:more_vert"
+              variant="plain"
+              v-bind="props"
+              title="more"
+          />
+        </template>
+        <v-list>
+          <v-list-item
+              :prepend-icon="darkTheme ? 'md:light_mode' : 'md:dark_mode'"
+              @click="emits('toggleDownTheme')"
+              title="Change theme"
+              class="text-none"
+          />
+          <v-list-item
+              prepend-icon="md:bug_report"
+              @click="emits('openBugReport')"
+              title="Bug report"
+              class="text-none"
+          />
+          <v-list-item
+              prepend-icon="md:download"
+              @click="emits('downloadChats')"
+              title="Download"
+              class="text-none"
+          />
+        </v-list>
+      </v-menu>
     </template>
   </v-toolbar>
 </template>
