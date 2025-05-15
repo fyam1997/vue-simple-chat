6fcad9f4abd1796ab6e556602514fac7575180da
fyam <fung19971115@gmail.com>
2025-05-16 04:04:29 +0800

    Add space to ChatMessageList for improved layout

--- src/simplechat/components/ChatMessageList.vue
+++ src/simplechat/components/ChatMessageList.vue
@@ -30,7 +30,7 @@ viewModel.scrollEvent.observe(() => {
           @deleteMessage="viewModel.deleteMessage(msg.id)"
           @insertBefore="viewModel.insertBefore(msg.id)"
           ref="messages-items"
-          class="w-100 pl-2 pr-2 pt-4"
+          class="w-100 pl-2 pr-2 pb-4"
       />
     </TransitionGroup>
   </div>
