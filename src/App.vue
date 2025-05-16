3384968da2e6ac0971b484de696a5706ca1ab9c0
fyam <fung19971115@gmail.com>
2025-05-17 02:53:35 +0800

    Update layouts

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import SimpleChat from "@/simplechat/SimpleChat.vue";
 import {ChatViewModel} from "@/simplechat/components/ChatViewModel.ts";
-import {computed, provide} from "vue";
+import {computed, provide, ref} from "vue";
 import {useWindowSize} from "@vueuse/core";
 import ChatConfigPanel from "@/simplechat/components/ChatConfigPanel.vue";
 
@@ -13,27 +13,51 @@ const theme = computed(() => {
 })
 
 const screenWidth = useWindowSize().width
-const largeScreen = computed(() => screenWidth.value >= 768)
-provide("largeScreen", largeScreen)
+const largeScreen = computed(() => screenWidth.value >= 700)
+
+const tab = ref("chat-panel")
 
 </script>
 
 <template>
   <v-app :theme="theme">
-    <div class="d-flex flex-row  w-100 h-100 ga-2 justify-center">
-      <ChatConfigPanel v-if="largeScreen" class="config-panel h-100 flex-grow-1 pa-4"/>
-      <v-divider v-if="largeScreen" vertical class="mt-4 mb-4"/>
-      <SimpleChat class="chat-panel h-100 flex-grow-1"/>
+    <div v-if="!largeScreen" class="w-100 h-100 d-flex flex-column">
+      <div>
+        <v-tabs
+            v-model="tab"
+            fixed-tabs
+            class="flex-grow-0"
+        >
+          <v-tab value="config-panel" class="text-none">Config</v-tab>
+          <v-tab value="chat-panel" class="text-none">Chats</v-tab>
+        </v-tabs>
+      </div>
+      <v-divider/>
+      <v-tabs-window v-model="tab" class="h-100 flex-grow-1">
+        <v-tabs-window-item value="config-panel" class="h-100">
+          <ChatConfigPanel class="h-100 pa-4"/>
+        </v-tabs-window-item>
+
+        <v-tabs-window-item value="chat-panel" class="h-100">
+          <SimpleChat class="h-100 pl-4 pr-4"/>
+        </v-tabs-window-item>
+      </v-tabs-window>
+    </div>
+
+    <div v-else class="w-100 h-100 d-flex flex-row ga-2 justify-center">
+      <ChatConfigPanel class="config-panel-large h-100 pa-4"/>
+      <v-divider vertical class="mt-4 mb-4"/>
+      <SimpleChat class="chat-panel-large flex-grow-1 h-100 pl-4 pr-4 pb-4"/>
     </div>
   </v-app>
 </template>
 
 <style scoped>
-.config-panel {
+.config-panel-large {
   max-width: 300px;
 }
 
-.chat-panel {
+.chat-panel-large {
   max-width: 600px;
 }
 </style>
--- src/simplechat/SimpleChat.vue
+++ src/simplechat/SimpleChat.vue
@@ -2,23 +2,14 @@
 
 import ChatMessageList from "@/simplechat/components/ChatMessageList.vue";
 import ChatInputField from "@/simplechat/components/ChatInputField.vue";
-import ChatToolbar from "@/simplechat/components/ChatToolbar.vue";
-import {inject} from "vue";
-import ChatInputConfigRow from "@/simplechat/components/ChatInputConfigRow.vue";
-
-const largeScreen = inject<number>("largeScreen")
 
 </script>
 
 <template>
   <div class="d-flex flex-column align-self-center">
-    <ChatToolbar v-if="!largeScreen"/>
     <ChatMessageList class="flex-grow-1 overflow-y-scroll"/>
     <v-divider/>
-    <div class="pt-4 pb-4 flex-grow-0">
-      <ChatInputConfigRow v-if="!largeScreen"/>
-      <ChatInputField/>
-    </div>
+    <ChatInputField class="pt-4 flex-grow-0"/>
   </div>
 </template>
 
